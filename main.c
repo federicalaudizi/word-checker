6a644a9ee7b86b47f66fe12cfb2ca486f0d010f7
federicalaudizi <federicalaudizi@gmail.com>
2022-07-19 20:21:17 +0200

    created first hash table

--- main.c
+++ main.c
@@ -5,28 +5,35 @@
 typedef struct { // Define the Hash Table Item here
     char *key;
     char *value;
-} Ht_item;
+} ht_item;
 
 typedef struct {
     // Contains an array of pointers to items
-    Ht_item **items;
+    ht_item **items;
     int size;
     int count;
 } HashTable;
 
+//ab
 HashTable *create_table(int size);
-unsigned long hashfun(unsigned char *str)
-Ht_item *create_item(char *key, char *value);
+
+int get_index(unsigned char *str, int size);
+
+unsigned long hash(unsigned char *str);
+
+ht_item* create_item(char *key, char value);
+
+void ht_insert(HashTable* table, char* key, char* value);
 
 
 int main() {
     int k;
     scanf("%d ", &k);
     unsigned char input_str[k];
-    unsigned char value;
-    int size = 50;
+    int value_index;
+    int init_hash_size = 50;
 
-    HashTable *table = *create_table(size);
+    HashTable table = *create_table(init_hash_size);
 
     while (1) {
         fgets(input_str, 1000, stdin);
@@ -40,11 +47,33 @@ int main() {
             printf("%s", "starting nuova partita");
             break;
         } else {
-            // step 1. reading list of legal words
+            // Create the item
+            ht_item* item = create_item(input_str, '0');
+            int index = get_index(input_str, init_hash_size);
 
-            value = hashfun(input_str);
-            create_item(input_str, value);
-            break;
+            HashTable* table = create_table(init_hash_size);
+
+            ht_item* current_item = table->items[index];
+            if (current_item == NULL) {
+                // Key does not exist.
+                if (table->count == table->size) {
+                    // Hash Table Full
+                    printf("Insert Error: Hash Table is full\n");               /*devo capire quando metto piu di 50 parole ammissibili mannaggia*/
+                    return;
+                }
+
+                // Insert directly
+                table->items[index] = item;
+                table->count++;
+            }
+
+
+
+
+            ht_item *item = create_item(input_str, '0');
+
+            // step 1. reading list of legal words
+            
         }
     }
 
@@ -52,9 +81,10 @@ int main() {
     return 0;
 }
 
-Ht_item *create_item(char *key, char *value) {  //ritorna il puntatore all'item che ho creato
+//creates hash item
+ht_item *create_item(char *key, char *value) {  //ritorna il puntatore all'item che ho creato
     // Creates a pointer to a new hash table item
-    Ht_item *item = malloc(sizeof(Ht_item));
+    ht_item *item = malloc(sizeof(ht_item));
     item->key = malloc(strlen(key) + 1);
     item->value = malloc(strlen(value) + 1);
     strcpy(item->key, key);
@@ -62,20 +92,36 @@ Ht_item *create_item(char *key, char *value) {  //ritorna il puntatore all'item 
     return item;
 }
 
+/** Creates empty hash table
+ *
+ * {
+ *      key1: value1,
+ *      key2: value2
+ * }
+ * index_val1 = hash(key1)
+ * value1 = items[index_val1]
+ */
+
+//creates hash table
 HashTable *create_table(int size) {
     // Creates a new HashTable
     HashTable *table = (HashTable *) malloc(sizeof(HashTable));
     table->size = size;
     table->count = 0;
     // dynamic array of pointers to items
-    table->items = calloc(table->size, sizeof(Ht_item *));
+    table->items = calloc(table->size, sizeof(ht_item *));
     for (int i = 0; i < table->size; i++) //sets all it’s items to NULL (Since they aren’t used)
         table->items[i] = NULL;
     return table;
 }
 
-unsigned long hashfun(unsigned char *str)
-{
+//mod n so that the index is actually in the table
+int get_index(unsigned char *str, int size) {
+    return hash(str) % size;
+}
+
+//proper hash function
+unsigned long hash(unsigned char *str) {
     unsigned long hash = 5381;
     int c;
 
@@ -84,3 +130,5 @@ unsigned long hashfun(unsigned char *str)
 
     return hash;
 }
+
+
